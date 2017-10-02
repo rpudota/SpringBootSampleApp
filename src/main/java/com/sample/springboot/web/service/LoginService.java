ea35167c7ae30d80cf7da924d9b8a6cdfa7b391a
rpudota <rohith.choudhary@gmail.com>
2017-10-02 12:54:10 -0700

    First commit

---
+++ src/main/java/com/sample/springboot/web/service/LoginService.java
@@ -0,0 +1,17 @@
+package com.sample.springboot.web.service;
+
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LoginService {
+
+    public boolean validateUser(String userid, String password) {
+        // in28minutes, dummy
+        return userid.equalsIgnoreCase("in28minutes")
+                && password.equalsIgnoreCase("dummy");
+    }
+
+}
+
